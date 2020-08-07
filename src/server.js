57035a304fe2f6f53e4dc9c6c17747e405d0e906
Vitoria Lopes <vilopesp@hotmail.com>
2020-08-07 20:37:49 -0300

    template engine study finalizado

--- src/server.js
+++ src/server.js
@@ -1,18 +1,55 @@
+const proffys = [
+    {
+       name: "Diego Fernandes",
+       avatar: "https://github.com/diego3g.png", 
+       whatsapp: "900000000", 
+       bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.00 pessoas já passaram por uma das minhas explosões.", 
+       subject: "Química", 
+       cost: "20", 
+       weekday: [0], 
+       time_from: [720], 
+       time_to: [1220]
+     },
+     {
+       name: "Mayk Brito",
+       avatar: "https://github.com/maykbrito.png", 
+       whatsapp: "900000000", 
+       bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.00 pessoas já passaram por uma das minhas explosões.", 
+       subject: "Química", 
+       cost: "20", 
+       weekday: [1], 
+       time_from: [720], 
+       time_to: [1220]
+     }
+   ]
+
+function pageLanding(req, res) {
+    return res.render("index.html")
+}
+
+function pageStudy(req, res) {
+    return res.render("study.html", { proffys })
+}
+
+function pageGiveClasses(req, res) {
+    return res.render("give-classes.html")
+}
+
 const express = require('express')
 const server = express()
 
-server.use(express.static("public"))
-
-.get("/", (req, res) => {
-    return res.sendFile(__dirname + "/views/index.html")
+// configurar nunjucks
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
 })
 
-.get("/study", (req, res) => {
-    return res.sendFile(__dirname + "/views/study.html")
-})
-
-.get("/give-classes", (req, res) => {
-    return res.sendFile(__dirname + "/views/give-classes.html")
-})
+server
+// configurar arquivos estáticos e rotas da aplicação
+.use(express.static("public"))
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
 
 .listen(5000)
